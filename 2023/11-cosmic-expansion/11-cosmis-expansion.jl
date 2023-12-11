d2b54599ebe1351f077d80d05fc3aab0835c0c32
Thomas Saigre <saigre@math.unistra.fr>
2023-12-11 09:24:03 +0100

    up #11

---
+++ 2023/11-cosmic-expansion/11-cosmis-expansion.jl
@@ -0,0 +1,61 @@
+# input_filename = "example.txt"
+input_filename = "input.txt"
+
+map = Dict('.' => 0, '#' => 1)
+
+function mat_of_input(input_filename)
+    input_file = open(input_filename)
+    lines = readlines(input_file)
+    close(input_file)
+    mat = zeros(Int, length(lines), length(lines[begin]))
+    for (i, line) in enumerate(lines)
+        for (j, char) in enumerate(line)
+            mat[i, j] = map[char]
+        end
+    end
+    return mat
+end
+
+function get_coords(mat, dist=1)
+    x = collect(1:size(mat, 1))
+    y = collect(1:size(mat, 2))
+
+    for (i, col) in enumerate(eachcol(mat))
+        if sum(col) == 0
+            x[i:end] .+= dist
+        end
+    end
+    for (j, row) in enumerate(eachrow(mat))
+        if sum(row) == 0
+            y[j:end] .+= dist
+        end
+    end
+    return x, y
+end
+
+
+function part_1(mat, dist=1)
+    coord_x, coord_y = get_coords(mat, dist)
+    galaxies = Tuple.(findall(x -> x != 0, mat))
+
+    sum = 0
+    for k in eachindex(galaxies)
+        for l in eachindex(galaxies)
+            if k < l
+                i1, j1 = galaxies[k]
+                i2, j2 = galaxies[l]
+                dist = abs(coord_y[i1] - coord_y[i2]) + abs(coord_x[j1] - coord_x[j2])
+                # println((i1, j1), "->", (i2, j2), ", dist = ", dist)
+                sum += dist
+            end
+        end
+    end
+    return sum
+end
+
+
+mat = mat_of_input(input_filename)
+I = part_1(mat)
+println("I = ", I)
+II = part_1(mat, 1000000-1)
+println("II = ", II)
