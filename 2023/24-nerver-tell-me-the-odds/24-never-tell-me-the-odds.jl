38963905f675a7b068d365dde252e3ab9f54d299
Thomas Saigre <saigre@math.unistra.fr>
2023-12-27 23:55:55 +0100

    attemp for #24.2 for it did not work :/ and I used another code 😠

--- 2023/24-nerver-tell-me-the-odds/24-never-tell-me-the-odds.jl
+++ 2023/24-nerver-tell-me-the-odds/24-never-tell-me-the-odds.jl
@@ -70,6 +70,37 @@ end
 
 hails = parse_input(input_filename)
 
+function perfect_throw(hails)
+
+    p1, v1 = hails[1]
+    p2, v2 = hails[2]
+    p3, v3 = hails[3]
+
+    c_z_12 = [v2[2] - v1[2], v1[1] - v2[1], 0            , p1[2] - p2[2], p2[1] - p1[1], 0            ]
+    c_z_13 = [v3[2] - v1[2], v1[1] - v3[1], 0            , p1[2] - p3[2], p3[1] - p1[1], 0            ]
+    c_y_12 = [v2[3] - v1[3], 0            , v1[1] - v2[1], p1[3] - p2[3], 0            , p2[1] - p1[1]]
+    c_y_13 = [v3[3] - v1[3], 0            , v1[1] - v3[1], p1[3] - p3[3], 0            , p3[1] - p1[1]]
+    c_x_12 = [0            , v2[3] - v1[3], v1[2] - v2[2], 0            , p1[3] - p2[3], p2[2] - p1[2]]
+    c_x_13 = [0            , v3[3] - v1[3], v1[2] - v3[2], 0            , p1[3] - p3[3], p3[2] - p1[2]]
+
+    vecs = (c_z_12, c_z_13, c_y_12, c_y_13, c_x_12, c_x_13)
+    A = vcat(transpose.(vecs)...)
+
+    rhs_z_12 = p1[2] * v1[1] - p2[2] * v2[1] - p1[1] * v1[2] + p2[1] * v2[2]
+    rhs_z_13 = p1[2] * v1[1] - p3[2] * v3[1] - p1[1] * v1[2] + p3[1] * v3[2]
+    rhs_y_12 = p1[3] * v1[1] - p2[3] * v2[1] - p1[1] * v1[3] + p2[1] * v2[3]
+    rhs_y_13 = p1[3] * v1[1] - p3[3] * v3[1] - p1[1] * v1[3] + p3[1] * v3[3]
+    rhs_x_12 = p1[3] * v1[2] - p2[3] * v2[2] - p1[2] * v1[3] + p2[2] * v2[3]
+    rhs_x_13 = p1[3] * v1[2] - p3[3] * v3[2] - p1[2] * v1[3] + p3[2] * v3[3]
+
+    rhs = [rhs_z_12, rhs_z_13, rhs_y_12, rhs_y_13, rhs_x_12, rhs_x_13]
+
+    s = float.(A) \ float.(rhs)
+
+    # bon bah ça marche pas... oopsi
+    return sum(floor.(Int, s[1:3]))
+end
+
 # for (i, h1) in enumerate(hails)
 #     for (j, h2) in enumerate(hails)
 #         if i >= j
@@ -80,3 +111,6 @@ hails = parse_input(input_filename)
 # end
 I = intersect_test_area(hails, 200000000000000, 400000000000000)
 println("Intersections: ", I)
+
+II = perfect_throw(collect(hails))
+println("Part 2 : ", II)
